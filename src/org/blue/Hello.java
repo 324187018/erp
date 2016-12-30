dd9f45e36dfeffb3b4f3d3afc43f69face9b6207
blue <324187018@qq.com>
2016-12-30 22:16:10 +0800

    三四岁

--- src/org/blue/Hello.java
+++ src/org/blue/Hello.java
@@ -1,10 +1,12 @@
 package org.blue;
 
 public class Hello {
-	public void say(String s){
+	public void say(String s) {
 		System.out.println(s);
+		System.out.println("人薩極傍。。。。。");
 	}
-    public static void mian(String[] args){
-      System.out.println("hello world");
-    }
+
+	public static void mian(String[] args) {
+		System.out.println("hello world");
+	}
 }
